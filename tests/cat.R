c6cf179803a383175e4b452a3dace179198501af
Yonghyun Kwon <yhkwon@iastate.edu>
2023-03-23 16:14:53 -0500

    Create cat.R

---
+++ tests/cat.R
@@ -0,0 +1,74 @@
+library(dplyr)
+library(cat)
+
+data(older)
+p = 6
+Y = do.call("rbind", apply(older, 1, function(x) matrix(rep(x[1:p], each = x[p+1]), nc = p)))
+Y = as.data.frame(Y)
+for(k in 1:p){
+  Y[[k]] = factor(Y[[k]])
+}
+# p = ncol(Y)
+
+(n = nrow(Y)); sum(older[,p+1])
+
+cand.edges = as.list(data.frame(combn(p, 2)))
+dp = doublep(Y, cand.edges)
+EFI = efi(Y, dp)
+estimate(EFI, "(V1 == 1) & (V2 == 2)")
+# with(imp, summary(glm((Var1 == 1) & (Var2 == 2) ~ 1, family = binomial(link = "identity"), weights = w)))
+
+
+# data(crimes)
+# 
+# p = 2
+# Y = do.call("rbind", apply(crimes, 1, function(x) matrix(rep(x[1:p], each = x[p+1]), nc = p)))
+# Y = as.data.frame(Y)
+# for(k in 1:p){
+#   Y[[k]] = factor(Y[[k]])
+# }
+# 
+# (n = nrow(Y)); sum(crimes[,p+1])
+# 
+# cand.edges = as.list(data.frame(combn(p, 2)))
+# dp = doublep(Y, cand.edges)
+# EFI = efi(Y, dp)
+# estimate(EFI, "(V1 == 1) & (V2 == 2)")
+
+data(belt)
+belt
+p = 6
+Y = do.call("rbind", apply(belt, 1, function(x) matrix(rep(x[1:p], each = x[p+1]), nc = p)))
+Y = as.data.frame(Y)
+for(k in 1:p){
+  Y[[k]] = factor(Y[[k]])
+}
+
+(n = nrow(Y)); sum(belt[,p+1])
+Y = Y[sample(1:n, 1000),]
+
+cand.edges = as.list(data.frame(combn(p, 2)))
+dp = doublep(Y, cand.edges)
+plot(dp)
+EFI = efi(Y, dp)
+estimate(EFI, "(V1 == 1) & (V2 == 2)")
+
+
+data(HairEyeColor)
+
+p = 3
+Y = do.call("rbind", apply(as.data.frame.table(HairEyeColor), 1, function(x) matrix(rep(x[1:p], each = x[p+1]), nc = p)))
+Y = as.data.frame(Y)
+for(k in 1:p){
+  Y[[k]] = factor(Y[[k]])
+  levels(Y[[k]]) <- as.character(1:nlevels(Y[[k]]))
+}
+(n = nrow(Y)); sum(HairEyeColor)
+delta = matrix(rbinom(n * p, 1, 0.9), nr = n, nc = p)
+Y[delta == 0] = NA
+
+cand.edges = as.list(data.frame(combn(p, 2)))
+dp = doublep(Y, cand.edges)
+plot(dp)
+EFI = efi(Y, dp)
+estimate(EFI, "(V1 == 1) & (V2 == 2)")
