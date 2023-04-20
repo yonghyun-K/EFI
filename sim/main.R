792a059994b118b1ae7dd3c61825eec984ccddc1
Yonghyun Kwon <yhkwon@iastate.edu>
2023-04-20 13:53:35 -0500

    Update main.R

--- sim/main.R
+++ sim/main.R
@@ -1,3 +1,5 @@
+require(R.utils)
+
 library(readr)
 library(tidyr)
 library(dplyr)
@@ -17,9 +19,16 @@ library(ggpubr)
 set.seed(123)
 SIMNUM = 200
 
-# data_name = "HairEyeColor"
-data_list = c("UCBAdmissions", "Titanic", "esoph", "PreSex", "car")
-# data_list = c("HairEyeColor", "Titanic")
+high_dim = T
+MAR = F
+
+# data_name = "chess_data"
+
+if(high_dim == F){
+  data_list = c("UCBAdmissions", "Titanic", "esoph", "PreSex", "car")
+}else{
+  data_list = c("spect_data", "mushroom_data", "promoters_data", "chess_data", "audiology")
+}
 
 timenow1 = Sys.time()
 timenow0 = gsub(' ', '_', gsub('[-:]', '', timenow1))
@@ -47,6 +56,7 @@ promoters_data <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-data
 colnames(promoters_data) <- c("Class", "id", "Sequence")
 promoters_data <- separate(promoters_data, Sequence, into = paste0("pos_", 1:57), sep = "")
 promoters_data <- promoters_data %>% dplyr::select(-id, -pos_1) %>% mutate_all(factor)
+promoters_data <- promoters_data[,1:15]
 
 mushroom_names <- c("class", "cap.shape", "cap.surface", "cap.color", "bruises", "odor",
                     "gill.attachment", "gill.spacing", "gill.size", "gill.color",
@@ -56,7 +66,6 @@ mushroom_names <- c("class", "cap.shape", "cap.surface", "cap.color", "bruises",
                     "ring.type", "spore.print.color", "population", "habitat")
 mushroom_data <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data", col_names = mushroom_names,
                           col_types = cols(.default = "f"))
-mushroom_data <- mushroom_data[,sapply(mushroom_data, nlevels) > 1]
 # lapply(mushroom_data, levels)
 
 column_names <- c("age_gt_60", "air", "airBoneGap", "ar_c", "ar_u", "bone", "boneAbnormal", "bser",
@@ -118,6 +127,10 @@ eval(parse(text = paste("df_true =", data_name)))
 df_true = df_true %>%
     mutate_if(is.ordered, factor, ordered = F)
 
+df_true <- df_true[,sapply(df_true, nlevels) > 1]
+
+modes = sapply(df_true, function(x) names(which.max(table(x))))
+
 # df_true = car
 p = ncol(df_true)
 n = nrow(df_true)
@@ -125,16 +138,14 @@ n = nrow(df_true)
 #   df_true[[k]] <- as.factor(df_true[[k]])
 # }
 
-modes = sapply(df_true, function(x) names(which.max(table(x))))
-
 levelone = names(which.max(summary(df_true[[1]])))
 trueval = mean(df_true[[1]] == levelone, na.rm = T)
 
 mis_rate_vec = seq(from = 0.1, to = 0.9, length = 9)
 
 oper <-
-  foreach(mis_rate_idx= 1:length(mis_rate_vec), .packages = c("CVXR", "mice", "missForest", "Amelia", "EFI","dplyr")) %:%
-  foreach(simnum = 1:SIMNUM, .combine='cbind', .packages = c("CVXR", "mice", "missForest", "Amelia", "EFI", "dplyr")) %dopar% {
+  foreach(mis_rate_idx= 1:length(mis_rate_vec), .packages = c("R.utils", "CVXR", "mice", "missForest", "Amelia", "EFI","dplyr")) %:%
+  foreach(simnum = 1:SIMNUM, .combine='cbind', .packages = c("R.utils", "CVXR", "mice", "missForest", "Amelia", "EFI", "dplyr")) %dopar% {
 
 # mis_rate_idx = 4
 #       for(simnum in 1:SIMNUM){
@@ -142,6 +153,7 @@ oper <-
     print(paste("mis_rate_idx = ", mis_rate_idx))
     print(paste("simnum = ", simnum))
     mis_rate = mis_rate_vec[mis_rate_idx]
+    # mis_rat = 0.9
     obs_rate = 1 - mis_rate
     # print(mis_rate)
 
@@ -151,135 +163,131 @@ oper <-
       print(cnt)
     df <- df_true
 
-    # MCAR
-    delta = sapply(1:p, function(x) rbinom(n, 1, obs_rate) == 1)
 
-    # MAR
-    # p2 = round(p / 2)
-    # delta = apply(df_true, 1, function(y){
-    #   deltav = integer(p)
-    #   tmpval = sample(1:p, 1)
-    #   tmpidx = (tmpval:(tmpval + p2 - 1)) %% p + 1
-    #   deltav[tmpidx] = 1
-    #   if(tmpval %% 2 == 0){
-    #     deltav[-tmpidx] = rbinom(p2,1,ifelse(y[tmpidx] == modes[tmpidx], min(c(3 * obs_rate, 1)), max(c(obs_rate / 3, 0))))[1:(p - p2)]
-    #   }else{
-    #     deltav[-tmpidx] = rbinom(p2,1,ifelse(y[tmpidx] == modes[tmpidx], max(c(obs_rate / 3, 0)), min(c(3 * obs_rate, 1))))[1:(p - p2)]
-    #   }
-    #   # deltav[-tmpidx] = rbinom(p2,1,ifelse(y[tmpidx] == 1, 0.1, 0.9))
-    #
-    #   deltav
-    # })
-    # delta = t(delta)
+    if(high_dim == F & MAR == F){
+      # MCAR
+      delta = sapply(1:p, function(x) rbinom(n, 1, obs_rate) == 1)
+    }else if(high_dim == T & MAR == F){
+      # MCAR - high dim
+      delta = matrix(1, nr =  n, nc = p)
+      delta[,1] = (rbinom(n, 1, obs_rate) == 1)
+    }else if(high_dim == F & MAR == T){
+      # MAR
+      p2 = round(p / 2)
+      delta = apply(df_true, 1, function(y){
+        deltav = integer(p)
+        tmpval = sample(1:p, 1)
+        tmpidx = (tmpval:(tmpval + p2 - 1)) %% p + 1
+        deltav[tmpidx] = 1
+        if(tmpval %% 2 == 0){
+          deltav[-tmpidx] = rbinom(p2,1,ifelse(y[tmpidx] == modes[tmpidx], min(c(3 * obs_rate, 1)), max(c(obs_rate / 3, 0))))[1:(p - p2)]
+        }else{
+          deltav[-tmpidx] = rbinom(p2,1,ifelse(y[tmpidx] == modes[tmpidx], max(c(obs_rate / 3, 0)), min(c(3 * obs_rate, 1))))[1:(p - p2)]
+        }
+        # deltav[-tmpidx] = rbinom(p2,1,ifelse(y[tmpidx] == 1, 0.1, 0.9))
 
-    # tmpidx = order(abs(cor(sweep(df_true, 2, modes, "=="))[1,]), decreasing = T)[2]
-    # delta = apply(df_true, 1, function(y){
-    #   deltav = rep(1, p)
-    #   deltav[1] = rbinom(1,1,ifelse(y[tmpidx] == modes[tmpidx], min(c(3 * obs_rate, 1)), max(c(obs_rate / 3, 0))))
-    #   deltav
-    # })
-    # delta = t(delta)
-    # mean(delta)
+        deltav
+      })
+      delta = t(delta)
+    }else if(high_dim == T & MAR == T){
+      # MAR - high dim
+      tmpidx = order(abs(cor(sweep(df_true, 2, modes, "=="))[1,]), decreasing = T)[2:5]
+      delta = apply(df_true, 1, function(y){
+        deltav = rep(1, p)
+        deltav[1] = rbinom(1,1,ifelse(y[sample(tmpidx, 1)] == modes[sample(tmpidx, 1)], min(c(3 * obs_rate, 1)), max(c(obs_rate / 3, 0))))
+        deltav
+      })
+      delta = t(delta)
+    }
 
     df[delta == 0] <- NA
 
     # print(sapply(df, function(x) sum(!is.na(unique(x)))))
-    if(all(sapply(df, function(x) sum(!is.na(unique(x)))) != 1)) break
-  }
+    if(all(sapply(df, function(x) sum(!is.na(unique(x)))) != 1) | cnt > 100) break
+    }
+    nacols = sapply(df, function(x) any(is.na(x)))
     print("Missing values are generated")
 
-    methods_mice = c("polyreg", "pmm", "cart", "lda")
-
-    micevals = c()
-    miceaccs = c()
-    for(method in methods_mice){
-      imp_mice <- tryCatch(mice(df, m = 5, seed = 123, printFlag = F, method = method),
-               error = function(e) {
-                 cat("An error occurred: ", conditionMessage(e))
-               })
-      if(!is.null(imp_mice)){
-        df_mice = complete(imp_mice,1:5)
-        micevals = c(micevals, mean(df_mice[[1]] == levelone))
-        miceaccs = c(miceaccs, mean(complete(imp_mice) == df_true))
-      }else{
-        micevals = NA
-        miceaccs = NA
+    if(high_dim == F){
+      methods_mice = c("polyreg", "pmm", "cart", "lda")
+      vals = c()
+      accs = c()
+      for(method in methods_mice){
+        imp_mice <- tryCatch(mice(df, m = 5, seed = 123, printFlag = F, method = method),
+                             error = function(e) {
+                               cat("An error occurred: ", conditionMessage(e))
+                             })
+        if(!is.null(imp_mice)){
+          df_mice = complete(imp_mice,1:5)
+          vals = c(vals, method = mean(df_mice[[1]] == levelone))
+          accs = c(accs, method = mean(complete(imp_mice)[,nacols,drop = F] == df_true[,nacols,drop = F]))
+        }else{
+          vals = c(vals, method = NA)
+          accs = c(accs, method = NA)
+        }
       }
+      print("MICE successfully done")
+    }else{
+
+      imp_missF <- tryCatch(withTimeout({missForest(cbind(1, df))}, timeout = 60),
+                            error = function(e) {
+                              cat("An error occurred: ", conditionMessage(e))
+                            })
+
+      # imp_missF <- missForest(cbind(1, df))
+      # df_missF = imp_missF$ximp[-1]
+      # print("missForest successfully done")
+
+      # imp_Amelia <- amelia(df, noms = names(df), p2s = 0)
+      # df_Amelia = do.call("rbind", imp_Amelia$imputations)
+      # print("Amelia successfully done")
+
+      # Amelia = mean(df_Amelia[[1]] == levelone, na.rm = T)
+
+      vals = c(missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][[1]] == levelone), NA))
+      accs = c(missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][,nacols,drop = F] == df_true[,nacols,drop = F]), NA))
     }
-    print("MICE successfully done")
 
-    # imp_mice_polyreg <- mice(df, m = 5, seed = 123, printFlag = F, method = "polyreg")
-    # df_mice_polyreg = complete(imp_mice_polyreg,1:5)
-    # print("MICE polyreg successfully done")
-    #
-    # imp_mice_pmm <- mice(df, m = 5, seed = 123, printFlag = F, method = "pmm")
-    # df_mice_pmm = complete(imp_mice_pmm,1:5)
-    # print("MICE pmm successfully done")
-    #
-    # imp_mice_cart <- mice(df, m = 5, seed = 123, printFlag = F, method = "cart")
-    # df_mice_cart = complete(imp_mice_cart,1:5)
-    # print("MICE cart successfully done")
-    #
-    # imp_mice_lda <- mice(df, m = 5, seed = 123, printFlag = F, method = "lda")
-    # df_mice_lda = complete(imp_mice_lda,1:5)
-    # print("MICE lda successfully done")
+    edges_list <- if(high_dim == T) apply(rbind(1, 2:p), 2, list) else
+      apply(combn(p, 2), 2, list)
 
-    # imp_missF <- missForest(cbind(1, df))
-    # df_missF = imp_missF$ximp[-1]
-    # print("missForest successfully done")
-
-    # imp_Amelia <- amelia(df, noms = names(df), p2s = 0)
-    # df_Amelia = do.call("rbind", imp_Amelia$imputations)
-    # print("Amelia successfully done")
-
-    # edges_list = apply(rbind(1, 2:p), 2, list)
-    edges_list = apply(combn(p, 2), 2, list)
-    dp = doublep(df, edges_list, R = 1)
-    imp_EFI = efi(df, dp)
-    print("EFI successfully done")
+    imp_EFI <- tryCatch(withTimeout({dp <- doublep(df, edges_list, R = 1)
+    efi(df, dp)}, timeout = 60),
+                          error = function(e) {
+                            cat("An error occurred: ", conditionMessage(e))
+                          })
+    if(!is.null(imp_EFI)){
+      complete_EFI = imp_EFI$imp %>% group_by(id) %>% summarize(maxw = max(w)) %>%
+        left_join(imp_EFI$imp, by = c("id", "maxw" = "w"), multiple = "first") %>%
+        select(-maxw, -Freq, -id)
+      EFIval = sum((imp_EFI$imp[[1]] == levelone) * ( imp_EFI$imp$w)) / n
+      EFIacc = mean(complete_EFI[,nacols,drop = F] == df_true[,nacols,drop = F])
+    }else{
+      EFIval = NA
+      EFIacc = NA
+    }
 
     complete_CC = df
     for (k in 1:p) {
       complete_CC[, k][is.na(complete_CC[, k])] <- modes[k]
     }
 
-    modeacc = mean(complete_CC == df_true)
+    modeacc = mean(complete_CC[,nacols,drop = F] == df_true[,nacols,drop = F])
+    modeval = mean(df[[1]] == levelone, na.rm = T)
 
-    complete_EFI = imp_EFI$imp %>% group_by(id) %>% summarize(maxw = max(w)) %>%
-      left_join(imp_EFI$imp, by = c("id", "maxw" = "w"), multiple = "first") %>%
-      select(-maxw, -Freq, -id)
-
-    if(nrow(complete_EFI)!= n){
-      print(complete_EFI)
-      print(paste("nrow(complete_EFI) = ", nrow(complete_EFI)))
-      EFIacc = NA
-    }else{
-      EFIacc = mean(complete_EFI == df_true)
-    }
-
-    result1 = c(CC = mean(df[[1]] == levelone, na.rm = T),
-               EFI = sum((imp_EFI$imp[[1]] == levelone) * ( imp_EFI$imp$w)) / n,
+    result1 = c(CC = modeval,
+               EFI = EFIval,
                # estimate(imp_EFI, "(Class_Values == \"unacc\")"),
-               # MICE = mean(df_mice[[1]] == levelone)
-               polyreg = micevals[1],
-               pmm = micevals[2],
-               cart = micevals[3],
-               lda = micevals[4]
-               # missF = mean(df_missF[[1]] == levelone)
-               # Amelia = mean(df_Amelia[[1]] == levelone, na.rm = T)
+               vals
                )
     result2 = c(mode = modeacc,
                 EFI = EFIacc,
-                polyreg = miceaccs[1],
-                pmm = miceaccs[2],
-                cart = miceaccs[3],
-                lda = miceaccs[4])
+                accs)
     c(result1, result2)
 
   }
 
 names(oper) <- mis_rate_vec
-
 
 oper1 = lapply(oper, function(x) x[1:(nrow(x) / 2),])
 oper2 = lapply(oper, function(x) x[(nrow(x) / 2 + 1):nrow(x),])
@@ -306,8 +314,8 @@ plot_res = ggplot(res_long, aes(x = missrate, y = value, color = method)) +
   scale_x_continuous(breaks = mis_rate_vec, labels = (function(x) sprintf("%.2f", x)) ) +
   labs(x = NULL, y = NULL) +
   ggtitle(data_name, subtitle = paste("n = ", n, ", p = ", p)) +
-  theme_bw() +
-  theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
+  ggplot2::theme_bw() +
+  ggplot2::theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
         panel.background = element_rect(fill = "white"),
         plot.title = element_text(size = 20, hjust = 0.5),
         legend.title = element_text(size = 16),
@@ -323,8 +331,8 @@ plot_res2 = ggplot(res2_long, aes(x = missrate, y = value, color = method)) +
   scale_x_continuous(breaks = mis_rate_vec, labels = (function(x) sprintf("%.2f", x)) ) +
   labs(x = NULL, y = NULL) +
   ggtitle(data_name, subtitle = paste("n = ", n, ", p = ", p)) +
-  theme_bw() +
-  theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
+  ggplot2::theme_bw() +
+  ggplot2::theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
         panel.background = element_rect(fill = "white"),
         plot.title = element_text(size = 20, hjust = 0.5),
         legend.title = element_text(size = 16),
@@ -366,8 +374,8 @@ for(idx in 1:length(res_list)){
     scale_x_continuous(breaks = mis_rate_vec, labels = (function(x) sprintf("%.2f", x)) ) +
     labs(x = NULL, y = NULL) +
     ggtitle(data_name, subtitle = paste("n = ", n, ", p = ", p)) +
-    theme_bw() +
-    theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
+    ggplot2::theme_bw() +
+    ggplot2::theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
           panel.background = element_rect(fill = "white"),
           plot.title = element_text(size = 20, hjust = 0.5),
           legend.title = element_text(size = 16),
@@ -379,8 +387,8 @@ for(idx in 1:length(res_list)){
     scale_x_continuous(breaks = mis_rate_vec, labels = (function(x) sprintf("%.2f", x)) ) +
     labs(x = NULL, y = NULL) +
     ggtitle(data_name, subtitle = paste("n = ", n, ", p = ", p)) +
-    theme_bw() +
-    theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
+    ggplot2::theme_bw() +
+    ggplot2::theme(panel.grid.major = element_line(color = "gray", linetype = "dashed"),
           panel.background = element_rect(fill = "white"),
           plot.title = element_text(size = 20, hjust = 0.5),
           legend.title = element_text(size = 16),
