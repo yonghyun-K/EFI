ca62ad0dc271156fb1c50fecaea9d06eb5f1eb15
Yonghyun Kwon <yhkwon@iastate.edu>
2023-06-29 23:39:54 -0500

    Update main.R

--- sim/main.R
+++ sim/main.R
@@ -18,13 +18,13 @@ library(ggplot2)
 library(ggpubr)
 
 set.seed(123)
-SIMNUM = 100
+SIMNUM = 50
 
-high_dim = F
+high_dim = T
 MAR = F
 
 
-# data_name = "audiology"
+if(interactive()) data_name = "chess_data"
 
 if(high_dim == F){
   data_list = c("esoph", "PreSex", "HairEyeColor", "UCBAdmissions", "Titanic")
@@ -41,68 +41,68 @@ setwd(timenow0)
 
 cores = min(detectCores() - 3, 100)
 myCluster <- makeCluster(cores, # number of cores to use
-                         type = "PSOCK",
-                         outfile = timenow) # type of cluster
+                         type = "PSOCK") # type of cluster
+                         # outfile = timenow)
 registerDoParallel(myCluster)
 
 if(!interactive()) sink(timenow, append=TRUE)
 
-# spect_names <- c("OVERALL_DIAGNOSIS", "F1R", "F1S", "F2R", "F2S", "F3R", "F3S", "F4R", "F4S", "F5R", "F5S", "F6R", "F6S", "F7R", "F7S", "F8R", "F8S", "F9R", "F9S", "F10R", "F10S", "F11R", "F11S", "F12R", "F12S", "F13R", "F13S", "F14R", "F14S", "F15R", "F15S", "F16R", "F16S", "F17R", "F17S", "F18R", "F18S")
-# spect_data_train <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/spect/SPECT.train", col_names = spect_names,
-#                              col_types = cols(.default = "f"))
-# spect_data_test <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/spect/SPECT.test", col_names = spect_names,
-#                             col_types = cols(.default = "f"))
-# spect_data = rbind(spect_data_train, spect_data_test)
-#
-# promoters_data <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/molecular-biology/promoter-gene-sequences/promoters.data", col_names=FALSE)
-# colnames(promoters_data) <- c("Class", "id", "Sequence")
-# promoters_data <- separate(promoters_data, Sequence, into = paste0("pos_", 1:57), sep = "")
-# promoters_data <- promoters_data %>% dplyr::select(-id, -pos_1) %>% mutate_all(factor)
-# promoters_data <- promoters_data[,1:15]
-#
-# mushroom_names <- c("class", "cap.shape", "cap.surface", "cap.color", "bruises", "odor",
-#                     "gill.attachment", "gill.spacing", "gill.size", "gill.color",
-#                     "stalk.shape", "stalk.root", "stalk.surface.above.ring",
-#                     "stalk.surface.below.ring", "stalk.color.above.ring",
-#                     "stalk.color.below.ring", "veil.type", "veil.color", "ring.number",
-#                     "ring.type", "spore.print.color", "population", "habitat")
-# mushroom_data <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data", col_names = mushroom_names,
-#                           col_types = cols(.default = "f"))
-# # lapply(mushroom_data, levels)
-#
-# column_names <- c("age_gt_60", "air", "airBoneGap", "ar_c", "ar_u", "bone", "boneAbnormal", "bser",
-#                   "history_buzzing", "history_dizziness", "history_fluctuating", "history_fullness",
-#                   "history_heredity", "history_nausea", "history_noise", "history_recruitment",
-#                   "history_ringing", "history_roaring", "history_vomiting", "late_wave_poor", "m_at_2k",
-#                   "m_cond_lt_1k", "m_gt_1k", "m_m_gt_2k", "m_m_sn", "m_m_sn_gt_1k", "m_m_sn_gt_2k",
-#                   "m_m_sn_gt_500", "m_p_sn_gt_2k", "m_s_gt_500", "m_s_sn", "m_s_sn_gt_1k", "m_s_sn_gt_2k",
-#                   "m_s_sn_gt_3k", "m_s_sn_gt_4k", "m_sn_2_3k", "m_sn_gt_1k", "m_sn_gt_2k", "m_sn_gt_3k",
-#                   "m_sn_gt_4k", "m_sn_gt_500", "m_sn_gt_6k", "m_sn_lt_1k", "m_sn_lt_2k", "m_sn_lt_3k",
-#                   "middle_wave_poor", "mod_gt_4k", "mod_mixed", "mod_s_mixed", "mod_s_sn_gt_500",
-#                   "mod_sn", "mod_sn_gt_1k", "mod_sn_gt_2k", "mod_sn_gt_3k", "mod_sn_gt_4k", "mod_sn_gt_500",
-#                   "notch_4k", "notch_at_4k", "o_ar_c", "o_ar_u", "s_sn_gt_1k", "s_sn_gt_2k", "s_sn_gt_4k",
-#                   "speech", "static_normal", "tymp", "viith_nerve_signs", "wave_V_delayed", "waveform_ItoV_prolonged",
-#                   "identifier", "class")
-# audiology <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.standardized.data",
-#                       col_types = cols(.default = "f"), col_names = column_names)
-# audiology <- (audiology %>% dplyr::select(-identifier))
-# audiology <- audiology[c(ncol(audiology), 1:(ncol(audiology)-1))]
-# # lapply(audiology, levels)
-#
-#
-# chess_data <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/chess/king-rook-vs-king-pawn/kr-vs-kp.data",
-#                        col_types = cols(.default = "f"))
-# colnames(chess_data) <- c("bkblk", "bknwy", "bkon8", "bkona", "bkspr", "bkxbq", "bkxcr", "bkxwp", "blxwp", "bxqsq",
-#          "cntxt", "dsopp", "dwipd", "hdchk", "katri", "mulch", "qxmsq", "r2ar8", "reskd", "reskr",
-#          "rimmx", "rkxwp", "rxmsq", "simpl", "skach", "skewr", "skrxp", "spcop", "stlmt", "thrsk",
-#          "wkcti", "wkna8", "wknck", "wkovl", "wkpos", "wtoeg", "won")
-# chess_data <- chess_data[c(ncol(chess_data), 1:(ncol(chess_data)-1))]
-#
-# car <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data",
-#                      col_names=c("buying", "maint", "doors", "persons", "lug_boot", "safety", "class_values"),
-#                      col_types = cols(.default = "f"))
-# colnames(car) <- c("Buying", "Maintenance", "Doors", "Persons", "Lug_Boot", "Safety", "Class_Values")
-# car <- car[c(ncol(car), 1:(ncol(car)-1))]
+spect_names <- c("OVERALL_DIAGNOSIS", "F1R", "F1S", "F2R", "F2S", "F3R", "F3S", "F4R", "F4S", "F5R", "F5S", "F6R", "F6S", "F7R", "F7S", "F8R", "F8S", "F9R", "F9S", "F10R", "F10S", "F11R", "F11S", "F12R", "F12S", "F13R", "F13S", "F14R", "F14S", "F15R", "F15S", "F16R", "F16S", "F17R", "F17S", "F18R", "F18S")
+spect_data_train <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/spect/SPECT.train", col_names = spect_names,
+                             col_types = cols(.default = "f"))
+spect_data_test <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/spect/SPECT.test", col_names = spect_names,
+                            col_types = cols(.default = "f"))
+spect_data = rbind(spect_data_train, spect_data_test)
+
+promoters_data <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/molecular-biology/promoter-gene-sequences/promoters.data", col_names=FALSE)
+colnames(promoters_data) <- c("Class", "id", "Sequence")
+promoters_data <- separate(promoters_data, Sequence, into = paste0("pos_", 1:57), sep = "")
+promoters_data <- promoters_data %>% dplyr::select(-id, -pos_1) %>% mutate_all(factor)
+promoters_data <- promoters_data[,1:15]
+
+mushroom_names <- c("class", "cap.shape", "cap.surface", "cap.color", "bruises", "odor",
+                    "gill.attachment", "gill.spacing", "gill.size", "gill.color",
+                    "stalk.shape", "stalk.root", "stalk.surface.above.ring",
+                    "stalk.surface.below.ring", "stalk.color.above.ring",
+                    "stalk.color.below.ring", "veil.type", "veil.color", "ring.number",
+                    "ring.type", "spore.print.color", "population", "habitat")
+mushroom_data <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data", col_names = mushroom_names,
+                          col_types = cols(.default = "f"))
+# lapply(mushroom_data, levels)
+
+column_names <- c("age_gt_60", "air", "airBoneGap", "ar_c", "ar_u", "bone", "boneAbnormal", "bser",
+                  "history_buzzing", "history_dizziness", "history_fluctuating", "history_fullness",
+                  "history_heredity", "history_nausea", "history_noise", "history_recruitment",
+                  "history_ringing", "history_roaring", "history_vomiting", "late_wave_poor", "m_at_2k",
+                  "m_cond_lt_1k", "m_gt_1k", "m_m_gt_2k", "m_m_sn", "m_m_sn_gt_1k", "m_m_sn_gt_2k",
+                  "m_m_sn_gt_500", "m_p_sn_gt_2k", "m_s_gt_500", "m_s_sn", "m_s_sn_gt_1k", "m_s_sn_gt_2k",
+                  "m_s_sn_gt_3k", "m_s_sn_gt_4k", "m_sn_2_3k", "m_sn_gt_1k", "m_sn_gt_2k", "m_sn_gt_3k",
+                  "m_sn_gt_4k", "m_sn_gt_500", "m_sn_gt_6k", "m_sn_lt_1k", "m_sn_lt_2k", "m_sn_lt_3k",
+                  "middle_wave_poor", "mod_gt_4k", "mod_mixed", "mod_s_mixed", "mod_s_sn_gt_500",
+                  "mod_sn", "mod_sn_gt_1k", "mod_sn_gt_2k", "mod_sn_gt_3k", "mod_sn_gt_4k", "mod_sn_gt_500",
+                  "notch_4k", "notch_at_4k", "o_ar_c", "o_ar_u", "s_sn_gt_1k", "s_sn_gt_2k", "s_sn_gt_4k",
+                  "speech", "static_normal", "tymp", "viith_nerve_signs", "wave_V_delayed", "waveform_ItoV_prolonged",
+                  "identifier", "class")
+audiology <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.standardized.data",
+                      col_types = cols(.default = "f"), col_names = column_names)
+audiology <- (audiology %>% dplyr::select(-identifier))
+audiology <- audiology[c(ncol(audiology), 1:(ncol(audiology)-1))]
+# lapply(audiology, levels)
+
+
+chess_data <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/chess/king-rook-vs-king-pawn/kr-vs-kp.data",
+                       col_types = cols(.default = "f"))
+colnames(chess_data) <- c("bkblk", "bknwy", "bkon8", "bkona", "bkspr", "bkxbq", "bkxcr", "bkxwp", "blxwp", "bxqsq",
+         "cntxt", "dsopp", "dwipd", "hdchk", "katri", "mulch", "qxmsq", "r2ar8", "reskd", "reskr",
+         "rimmx", "rkxwp", "rxmsq", "simpl", "skach", "skewr", "skrxp", "spcop", "stlmt", "thrsk",
+         "wkcti", "wkna8", "wknck", "wkovl", "wkpos", "wtoeg", "won")
+chess_data <- chess_data[c(ncol(chess_data), 1:(ncol(chess_data)-1))]
+
+car <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data",
+                     col_names=c("buying", "maint", "doors", "persons", "lug_boot", "safety", "class_values"),
+                     col_types = cols(.default = "f"))
+colnames(car) <- c("Buying", "Maintenance", "Doors", "Persons", "Lug_Boot", "Safety", "Class_Values")
+car <- car[c(ncol(car), 1:(ncol(car)-1))]
 
 data(HairEyeColor, Titanic, UCBAdmissions, esoph)
 
@@ -135,13 +135,15 @@ modes = sapply(df_true, function(x) names(which.max(table(x))))
 
 if(high_dim == T) df_true <- df_true[,mapply(function(x, y) sum(x == y), df_true, modes) / nrow(df_true) < 0.9]
 
+modes = sapply(df_true, function(x) names(which.max(table(x))))
+
 # df_true = car
 p = ncol(df_true)
 n = nrow(df_true)
 # for(k in 1:p){
 #   df_true[[k]] <- as.factor(df_true[[k]])
 # }
-modes = sapply(df_true, function(x) names(which.max(table(x))))
+# modes = sapply(df_true, function(x) names(which.max(table(x))))
 
 levelone = names(which.max(summary(df_true[[1]])))
 trueval = mean(df_true[[1]] == levelone, na.rm = T)
@@ -234,66 +236,45 @@ oper <-
     nacols = sapply(df, function(x) any(is.na(x)))
     print("Missing values are generated")
 
-    if(high_dim == F){
-      methods_mice = c("polyreg", "pmm", "cart", "lda")
-      vals = c()
-      accs = c()
-      for(method in methods_mice){
-        imp_mice <- tryCatch(mice(df, m = 5, seed = 123, printFlag = F, method = method),
-                             error = function(e) {
-                               cat("An error occurred in MICE: ", conditionMessage(e))
-                             })
-        if(!is.null(imp_mice)){
-          df_mice = complete(imp_mice,1:5)
-          valmice = mean(df_mice[[1]] == levelone); names(valmice) <- method
-          accmice = mean(complete(imp_mice)[is.na(df[,1]),1, drop = F] == df_true[is.na(df[,1]),1, drop = F]); names(accmice) <- method
-          vals = c(vals, valmice)
-          accs = c(accs, accmice)
-        }else{
-          vals = c(vals, method = NA)
-          accs = c(accs, method = NA)
-        }
+    methods_mice = c("polyreg", "pmm", "cart", "lda")
+    vals = c()
+    accs = c()
+    for(method in methods_mice){
+      imp_mice <- tryCatch(mice(df, m = 5, seed = 123, printFlag = F, method = method),
+                           error = function(e) {
+                             cat("An error occurred in MICE: ", conditionMessage(e))
+                           })
+      if(!is.null(imp_mice)){
+        df_mice = complete(imp_mice,1:5)
+        valmice = mean(df_mice[[1]] == levelone); names(valmice) <- method
+        accmice = mean(complete(imp_mice)[is.na(df[,1]),1, drop = F] == df_true[is.na(df[,1]),1, drop = F]); names(accmice) <- method
+        vals = c(vals, valmice)
+        accs = c(accs, accmice)
+      }else{
+        vals = c(vals, method = NA)
+        accs = c(accs, method = NA)
       }
-      print("MICE successfully done")
+    }
+    print("MICE successfully done")
 
-      imp_missF <- tryCatch(withTimeout({missForest(cbind(1, df))}, timeout = 60 * 10),
-                            error = function(e) {
-                              cat("An error occurred in missF: ", conditionMessage(e))
-                            })
-      if(!is.null(imp_missF)) df_missF = imp_missF$ximp[-1]
+    imp_missF <- tryCatch(withTimeout({missForest(cbind(1, df))}, timeout = 60 * 10),
+                          error = function(e) {
+                            cat("An error occurred in missF: ", conditionMessage(e))
+                          })
+    if(!is.null(imp_missF)) df_missF = imp_missF$ximp[-1]
 
-      vals = c(vals, missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][[1]] == levelone), NA))
-      accs = c(accs, missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][is.na(df[,1]), 1, drop = F] == df_true[is.na(df[,1]),1, drop = F]), NA))
+    vals = c(vals, missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][[1]] == levelone), NA))
+    accs = c(accs, missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][is.na(df[,1]), 1, drop = F] == df_true[is.na(df[,1]),1, drop = F]), NA))
 
-      if(MAR){
+    if(high_dim == F){
       imp_Amelia <- tryCatch(withTimeout({amelia(df, noms = names(df), p2s = 0)}, timeout = 60 * 10),
-                            error = function(e) {
-                              cat("An error occurred in Amelia: ", conditionMessage(e))
-                            })
+                             error = function(e) {
+                               cat("An error occurred in Amelia: ", conditionMessage(e))
+                             })
       if(!is.null(imp_Amelia)) df_Amelia = do.call("rbind", imp_Amelia$imputations)
 
       vals = c(vals, Amelia = ifelse(!is.null(imp_Amelia), mean(df_Amelia[[1]] == levelone), NA))
       accs = c(accs, Amelia = ifelse(!is.null(imp_Amelia), mean(imp_Amelia$imputations[[1]][is.na(df[,1]), 1, drop = F] == df_true[is.na(df[,1]),1, drop = F]), NA))
-      }
-    }else{
-
-      imp_missF <- tryCatch(withTimeout({missForest(cbind(1, df))}, timeout = 60 * 10),
-                            error = function(e) {
-                              cat("An error occurred in missF: ", conditionMessage(e))
-                            })
-
-      # imp_missF <- missForest(cbind(1, df))
-      # df_missF = imp_missF$ximp[-1]
-      # print("missForest successfully done")
-
-      # imp_Amelia <- amelia(df, noms = names(df), p2s = 0)
-      # df_Amelia = do.call("rbind", imp_Amelia$imputations)
-      # print("Amelia successfully done")
-
-      # Amelia = mean(df_Amelia[[1]] == levelone, na.rm = T)
-
-      vals = c(missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][[1]] == levelone), NA))
-      accs = c(missF = ifelse(!is.null(imp_missF), mean(imp_missF$ximp[-1][is.na(df[,1]), 1, drop = F] == df_true[is.na(df[,1]),1, drop = F]), NA))
     }
 
     complete_CC = df
@@ -309,6 +290,7 @@ oper <-
     #   nacols <- 1
     # }
 
+
     if(high_dim == T){
       # varidx =  combn(p, 2)
       varidx =  combn(p, 2)[,1:(p-1)]
@@ -322,7 +304,7 @@ oper <-
           edges_list[[itmp]] <- tmplist
           itmp = itmp + 1
         }
-        if(itmp > 100) break
+        if(itmp > 20) break
       }
 
     }else{
@@ -348,7 +330,7 @@ oper <-
                           error = function(e) {
                             cat("An error occurred in dp: ", conditionMessage(e))
                           })
-
+    # dp$weightveclist
     # dp$edges_list1
 
     # order(abs(cor(df_true[,1] == 1, df_true == 1)), decreasing = T)
